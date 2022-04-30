d68fae722b237973e16c3a94513d2fe144ac15af
il9 <ilya33113@gmail.com>
2022-05-01 01:59:11 +0300

    s

--- Мат програ/Лабы/ЛР №2/Lab2/Auxil.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cstdlib> 
-namespace auxil
-{
-	void start(); // старт генератора сл. чисел 
-	double dget(double rmin, double rmax); // получить случайное число 
-	int iget(int rmin, int rmax); // получить случайное число 
-};
